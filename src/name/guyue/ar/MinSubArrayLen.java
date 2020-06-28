253c4c416bfc89a6e5bf7eab14de2c14aa761519
hujia <hujia@topjoy.com>
2020-06-28 21:45:23 +0800

    LeetCode 209

---
+++ src/name/guyue/ar/MinSubArrayLen.java
@@ -0,0 +1,32 @@
+package name.guyue.ar;
+
+import name.guyue.Asserts;
+
+/**
+ * @author hujia
+ * @since 2020/6/28, Sun
+ */
+public class MinSubArrayLen {
+
+    public int minSubArrayLen(int s, int[] nums) {
+        int start = 0;
+        int length = 0;
+        int sum = 0;
+        for (int end = 0; end < nums.length; end++) {
+            sum += nums[end];
+            while ((sum - nums[start]) >= s) {
+                sum -= nums[start];
+                start++;
+            }
+            if (sum >= s) {
+                length = length == 0 ? end - start + 1 : Math.min(end - start + 1, length);
+            }
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        MinSubArrayLen func = new MinSubArrayLen();
+        Asserts.equals(2, func.minSubArrayLen(7, new int[] {2,3,1,2,4,3}));
+    }
+}
